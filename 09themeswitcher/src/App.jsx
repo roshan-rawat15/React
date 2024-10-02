e65d1a1dcb9829df675dc923b68665d1eba5ac8f
Roshan Kumar Rawat <165247276+Roshankumarrawat@users.noreply.github.com>
2024-10-02 22:38:40 +0530

    Store complete

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -1,9 +1,9 @@
-
 import { useEffect, useState } from 'react'
 import './App.css'
-import { ThemeProvider } from './contexts/theme'
+
 import ThemeBtn from './components/ThemeBtn'
 import Card from './components/Card'
+import { ThemeProvider } from './context/theme'
 
 function App() {
   const [themeMode, setThemeMode] = useState("light")
--- 09themeswitcher/src/components/ThemeBtn.jsx
+++ 09themeswitcher/src/components/ThemeBtn.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import useTheme from '../contexts/theme';
+import useTheme from '../context/theme'
+
 
 export default function ThemeBtn() {
-    
     const {themeMode, lightTheme, darkTheme} = useTheme()
     const onChangeBtn = (e) => {
         const darkModeStatus = e.currentTarget.checked
